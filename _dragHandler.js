c1249b259c1e7ad10ee758f6b7582cda2835b209
ahmedakbayir <ahmedakbayir@gmail.com>
2025-10-06 01:43:15 +0300

    yorumlar kaldırıldı

--- _dragHandler.js
+++ _dragHandler.js
@@ -2,9 +2,3 @@
 // interactionManager tarafından çağrılır.
 // Şimdilik boş bırakılabilir ve mantık interactionManager içinde kalabilir.
 // Proje daha da büyürse, bu ayrımı yapmak faydalı olacaktır.
-
-//yeni bir test
-
-// ikinci bir test
-
-//ücüncü test ile sadece 1 dosya göndereceğim. diğer duracak.
