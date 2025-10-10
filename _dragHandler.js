c0632c18128a78e7895126acf645f3a610ce16c1
ahmedakbayir <ahmedakbayir@gmail.com>
2025-10-10 16:11:39 +0300

    tüm dosya içerikleri yenilendi

--- _dragHandler.js
+++
@@ -1,4 +0,0 @@
-// Bu dosya sürükleme mantığını soyutlar.
-// interactionManager tarafından çağrılır.
-// Şimdilik boş bırakılabilir ve mantık interactionManager içinde kalabilir.
-// Proje daha da büyürse, bu ayrımı yapmak faydalı olacaktır.
