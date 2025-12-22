5b4b03e0f425eb4d38dc81840a0ba73ee53b9dc9
Ahmet Akbayır <ahmedakbayir@gmail.com>
2025-12-23 00:33:13 +0300

    sayaç sonrası

--- plumbing_v2/interactions/interaction-manager.js
+++ plumbing_v2/interactions/interaction-manager.js
@@ -1316,14 +1316,8 @@ export class InteractionManager {
         const boru = createBoru(this.boruBaslangic.nokta, point, 'STANDART');
         boru.floorId = state.currentFloorId;
 
-        // ✨ Başlangıç kaynağının rengini devral
-        if (this.boruBaslangic.kaynakTip === 'sayac') {
-            // Sayaç çıkışından başlıyorsa TURQUAZ
-            boru.colorGroup = 'TURQUAZ';
-        } else if (this.boruBaslangic.kaynakColorGroup) {
-            // Kaynak boru rengini kullan (split sonrası renk devam etsin)
-            boru.colorGroup = this.boruBaslangic.kaynakColorGroup;
-        }
+        boru.colorGroup = this.boruBaslangic.kaynakColorGroup || 'YELLOW';
+
 
         if (this.boruBaslangic.kaynakId) {
             boru.setBaslangicBaglanti(
