3068dca8500c43cf148bdf4de75c9b54026f2701
AhmetAkbayir <ahmedakbayir@gmail.com>
2026-05-13 16:53:24 +0300

    emn.v

--- plumbing_v2/renderer/renderer-labels.js
+++ plumbing_v2/renderer/renderer-labels.js
@@ -951,7 +951,7 @@ export const LabelMixin = {
             }
 
         } else if (vt === 'EMNIYET') {
-            lines.push({ text: 'Vana', sub: true });
+            lines.push({ text: 'Emn.V', sub: true });
 
         } else if (vt === 'SELENOID') {
             lines.push({ text: 'Selenoid Vana', sub: true });
